09506613903468c32a4b62d13f996f4d1947d391
bhavishaysikka <39116684+bhavishaysikka@users.noreply.github.com>
2023-11-03 16:38:35 -0400

    Backup script

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+src_dir=/home/ubuntu/scripts/
+tgt_dir=/home/ubuntu/backups/
+
+curr_timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
+backup_file=$tgt_dir/$curr_timestamp.tgz
+
+echo "Taking backup on $curr_timestamp" 
+
+#echo "$backup_file"
+
+tar czf $backup_file --absolute-names $scr_dir
+
+echo "backup complete"
+
