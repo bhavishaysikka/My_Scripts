ddbf7b2e3fb241a0cd9e2a05584a390f3901b86d
bhavishaysikka <39116684+bhavishaysikka@users.noreply.github.com>
2023-11-03 16:31:35 -0400

    Add files via upload
    Basic shell script

---
+++ devops_first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello I am $1"
+echo "Welcome to scripting learnings, Who is this ?"
+read name
+echo "Hello ${name} !"
+sleep 2
+
+echo "Good to see you here"
+sleep 2
+echo "All the best for learnings" 
